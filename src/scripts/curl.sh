787180d3d91c001466f45daf5380d7e0bf303cf0
jeff-knurek <jeff.knurek@ezypay.com>
2024-02-10 12:19:06 +0800

    cleanup

--- src/scripts/curl.sh
+++ src/scripts/curl.sh
@@ -13,15 +13,16 @@ echo "repo_url:   $CIRCLE_REPOSITORY_URL"
 echo "pipeline_id: $CIRCLE_BUILD_NUM"
 echo "ref_name:   $CIRCLE_USERNAME"
 echo "start_time: $start_time"
-echo ""
+echo "-------------"
 
 RESPONSE=$(curl "$DEVLAKE_ENDPOINT/api/rest/plugins/webhook/connections/1/deployments" -H "Authorization: Bearer $DEVLAKE_API_KEY" -d "{
+    \"commit_sha\":\"$CIRCLE_SHA1\",
+    \"repo_url\":\"$CIRCLE_REPOSITORY_URL\",
     \"pipeline_id\":\"$CIRCLE_BUILD_NUM\",
     \"ref_name\":\"$CIRCLE_USERNAME\",
     \"start_time\":\"$start_time\"
 }")
+echo "-------------"
 echo "$RESPONSE"
-
-echo "$PARAM_FAIL_BUILD"
-
+# check if should fail the build if the respose was not successful
 if [[ $(jq -r '.success' <<< "$RESPONSE") != "true" ]] && [ "$PARAM_FAIL_BUILD" -eq 1 ]; then exit 42; fi
