ba7f2153fc9304f906bea7f62f7bc9dbeb50d248
jeff-knurek <jeff.knurek@ezypay.com>
2024-02-10 10:43:49 +0800

    found th eproblem

--- src/scripts/curl.sh
+++ src/scripts/curl.sh
@@ -6,10 +6,10 @@ CONTEXT=$(circleci env subst "${GET_CONTEXT}")
 
 echo "context: ${CONTEXT}"
 # TODO: get start time from before the deployment started with a different orb step
-start_time=${date '+%Y-%m-%dT%H:%M:%S%z'}
+start_time=`date '+%Y-%m-%dT%H:%M:%S%z'`
 echo "reporting deployment"
 echo "commit_sha: $CIRCLE_SHA1"
-echo "repo_url:   $CIRCLE_REPOSI"TORY_URL"
+echo "repo_url:   $CIRCLE_REPOSITORY_URL"
 echo "pipeline_id: $CIRCLE_BUILD_NUM"
 echo "ref_name:   $CIRCLE_USERNAME"
-echo $start_time
+echo "start_time: $start_time"
