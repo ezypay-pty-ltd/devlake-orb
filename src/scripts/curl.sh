01a5c3a19da8df8726f7ae1f480c2ed9aa74a261
jeff-knurek <jeff.knurek@ezypay.com>
2024-02-10 11:28:21 +0800

    rename env var for context, then also change script usage

--- src/scripts/curl.sh
+++ src/scripts/curl.sh
@@ -7,7 +7,7 @@
 start_time=$(date '+%Y-%m-%dT%H:%M:%S%z')
 
 echo "reporting deployment"
-echo "curl $PARAM_ENDPOINT/api/rest/plugins/webhook/connections/1/deployments -X 'POST' -H 'Authorization: Bearer $PARAM_API_KEY'"
+echo "curl $ENDPOINT/api/rest/plugins/webhook/connections/1/deployments -X 'POST' -H 'Authorization: Bearer $API_KEY'"
 echo ""
 echo "commit_sha: $CIRCLE_SHA1"
 echo "repo_url:   $CIRCLE_REPOSITORY_URL"
@@ -15,7 +15,7 @@ echo "pipeline_id: $CIRCLE_BUILD_NUM"
 echo "ref_name:   $CIRCLE_USERNAME"
 echo "start_time: $start_time"
 
-curl -v "$ENDPOINT/api/rest/plugins/webhook/connections/1/deployments" -X 'POST' -H "Authorization: Bearer $PARAM_API_KEY" -d "{
+curl -v "$ENDPOINT/api/rest/plugins/webhook/connections/1/deployments" -X 'POST' -H "Authorization: Bearer $API_KEY" -d "{
     \"pipeline_id\":\"$CIRCLE_BUILD_NUM\",
     \"ref_name\":\"$CIRCLE_USERNAME\",
     \"start_time\":\"$start_time\"
